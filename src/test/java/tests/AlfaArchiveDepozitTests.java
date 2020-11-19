754d5e504c875b63d82cfbb8931372d37f496aaf
Anton Babushking <napishi_antonu@mail.ru>
2020-11-19 14:41:57 +0300

    alfa test

---
+++ src/test/java/tests/AlfaArchiveDepozitTests.java
@@ -0,0 +1,25 @@
+package tests;
+
+import com.codeborne.selenide.ElementsCollection;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AlfaArchiveDepozitTests {
+
+    @Test
+    void TestAlfa() {
+        open("https://alfabank.ru/");
+        $(byText("Вклады")).click();
+        $("body").shouldHave(text("Накопительные продукты"));
+        $$(byText("Депозиты")).find(visible).parent().click();
+        $$(byText("Архивные счета и депозиты")).find(visible).parent().click();
+        $$(byText("Депозиты")).find(visible).parent().click();
+        $("body").shouldHave(text("Вклады"));
+        $$("div[data-widget-name=\"CatalogCard\"]").shouldHaveSize(5);
+    }
+
+}
